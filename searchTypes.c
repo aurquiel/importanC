98369373276e4b262843177c74cdb649bd3a1451
Edgar Gomez <aurquiel@gmail.com>
2018-06-30 08:14:39 -0400

    Update searchTypes.c

--- searchTypes.c
+++ searchTypes.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 int linearSearch(int *pArray, size_t len, int long long value);
-int busquedadBinaria(int pArray[], size_t len, int long long value);
+int busquedaBinaria(int *myArray, size_t len, int key);
 
 int main()
 {
     int myArray[10] = {0,1,2,3,4,5,6,7,8,9};
 
     printf("%d\n",linearSearch(myArray,10,4));
-    printf("%d\n", busquedadBinaria(myArray,10,3));
+    printf("%d\n", busquedaBinaria(myArray,10,3));
 
     return 0;
 }
@@ -23,20 +23,18 @@ int linearSearch(int pArray[], size_t len, int long long value)
     return -1;
 }
 
-int busquedadBinaria(int pArray[], size_t len, int long long value)
+int busquedaBinaria(int *myArray, size_t len, int key)
 {
-    int limiteZ = 0, limiteD = len;
-    while(limiteZ < limiteD)
+    int mid = 0, low = 0, high = len -1;
+    while(low <= high)
     {
-        int mid = (limiteZ+limiteD)/2;
-
-        if(pArray[mid] == value)
-            return ++mid;
-        else if (pArray[mid] < value)
-            limiteZ = mid + 1;
+        mid = (high+low)/2;
+        if (*(myArray+mid) == key)
+            return mid;
+        else if (*(myArray+mid) < key)
+            low = mid + 1;
         else
-            limiteD = mid - 1;
-
+            high = mid - 1;
     }
     return -1;
 }
