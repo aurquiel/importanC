2ab750f4f71d354a0f7fd3c14af30d50810a884e
Edgar Gomez <aurquiel@gmail.com>
2018-07-01 19:54:12 -0400

    Update sortAlgorithms.c

--- sortAlgorithms.c
+++ sortAlgorithms.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void bubbleSort(int *myArray, size_t len);
+void quickSort(int *myArray, size_t left, size_t right);
 
 int main()
 {
@@ -15,6 +16,17 @@ int main()
 
     for (int i = 0; i<10; i++)
         printf("%d\t",myArra2[i]);
+    
+    int myArray[10] = { 3434,34343,665,6787,3423,4556,32323,787,1223};
+
+    for(int i = 0; i<8; i++)
+        printf("%d ",myArray[i]);
+
+    printf("\n");
+    quickSort(myArray,0,8);
+
+    for(int i = 0; i<8; i++)
+        printf("%d ",myArray[i]);
 
     return 0;
 }
@@ -30,4 +42,35 @@ void bubbleSort(int *myArray, size_t len)
                 myArray[j+1]    ^=  myArray[j];
                 myArray[j]      ^=  myArray[j+1];
             }
+}
+
+void quickSort(int *myArray, size_t left, size_t right)
+{
+    int i = left, j = right;
+    int tmp;
+    int pivot = myArray[(left + right) / 2];
+
+    /* partition */
+
+    while (i <= j)
+    {
+        while (myArray[i] < pivot)
+            i++;
+
+        while (myArray[j] > pivot)
+            j--;
+
+        if (i <= j)
+        {
+            tmp = myArray[i];
+            myArray[i++] = myArray[j];
+            myArray[j--] = tmp;
+        }
+    }
+
+    /* recursion */
+    if (left < j)
+        quickSort(myArray, left, j);
+    if (i < right)
+        quickSort(myArray, i, right);
 }
