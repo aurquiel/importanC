a7b14925229a899736fa68c5c47cdc724135f2ab
Edgar Gomez <aurquiel@gmail.com>
2018-07-27 17:22:29 -0400

    Update miAjedrez.c

--- miAjedrez.c
+++ miAjedrez.c
@@ -5,7 +5,10 @@
 const uint8_t TableroLineaTamanio = 8;
 const uint8_t PeonesNumero = 8;
 const uint8_t TorresNumero = 2;
+const uint8_t CaballosNumero = 2;
 const uint8_t AlfilesNumero = 2;
+const uint8_t DamaNumero = 1;
+const uint8_t ReyNumero = 1;
 
 enum COLOR {BLANCO,NEGRO};
 
@@ -28,17 +31,17 @@ typedef struct TorreEstructura{
     uint8_t Color;
 }TorreEstructura;
 
-typedef struct AlfilEstructura{
-    uint8_t PosicionVertical;
-    uint8_t PosicionHorizontal;
-    uint8_t Color;
-}AlfilEstructura;
-
 typedef struct CaballoEstructura{
     uint8_t PosicionVertical;
     uint8_t PosicionHorizontal;
     uint8_t Color;
 }CaballoEstructura;
+
+typedef struct AlfilEstructura{
+    uint8_t PosicionVertical;
+    uint8_t PosicionHorizontal;
+    uint8_t Color;
+}AlfilEstructura;
 
 typedef struct DamaEstructura{
     uint8_t PosicionVertical;
@@ -130,13 +133,47 @@ TorreEstructura** TorresInicializador(uint8_t colorSeleccionado)
                 else if (BLANCO == colorSeleccionado)
                 {
                     Torres[i]->PosicionVertical = 8;     //Posiciones por defecto de los peones blancos
-                    Torres[i]->PosicionHorizontal = 'h' + i*7;
+                    Torres[i]->PosicionHorizontal = 'a' + i*7;
                 }
             }
             else
                 exit(-1);
         }
         return Torres;
+    }
+    else
+        exit(-1);
+}
+
+CaballoEstructura** CaballosInicializador(uint8_t colorSeleccionado)
+{
+    //Alojar un apuntador de apuntadores de estructuras
+    CaballoEstructura **Caballos = calloc(sizeof(CaballoEstructura*), CaballosNumero);
+
+    if(Caballos)
+    {
+        //Alojar cada peon inidividualmente
+        for(uint8_t i = 0; i<CaballosNumero ; i++)
+        {
+            Caballos[i] = calloc(sizeof(CaballoEstructura),1);
+            if(Caballos[i])
+            {
+                Caballos[i]->Color = colorSeleccionado;
+                if (NEGRO == colorSeleccionado)
+                {
+                    Caballos[i]->PosicionVertical = 1;     //Posiciones opr defecto de los peones negros
+                    Caballos[i]->PosicionHorizontal = 'b' + i*5;
+                }
+                else if (BLANCO == colorSeleccionado)
+                {
+                    Caballos[i]->PosicionVertical = 8;     //Posiciones por defecto de los peones blancos
+                    Caballos[i]->PosicionHorizontal = 'b' + i*5;
+                }
+            }
+            else
+                exit(-1);
+        }
+        return Caballos;
     }
     else
         exit(-1);
@@ -150,7 +187,7 @@ AlfilEstructura** AlfilesInicializador(uint8_t colorSeleccionado)
     if(Alfiles)
     {
         //Alojar cada peon inidividualmente
-        for(uint8_t i = 0; i<PeonesNumero ; i++)
+        for(uint8_t i = 0; i<AlfilesNumero ; i++)
         {
             Alfiles[i] = calloc(sizeof(AlfilEstructura),1);
             if(Alfiles[i])
@@ -159,18 +196,52 @@ AlfilEstructura** AlfilesInicializador(uint8_t colorSeleccionado)
                 if (NEGRO == colorSeleccionado)
                 {
                     Alfiles[i]->PosicionVertical = 1;     //Posiciones opr defecto de los peones negros
-                    Alfiles[i]->PosicionHorizontal = 'b' + i*6;
+                    Alfiles[i]->PosicionHorizontal = 'c' + i*3;
                 }
                 else if (BLANCO == colorSeleccionado)
                 {
                     Alfiles[i]->PosicionVertical = 8;     //Posiciones por defecto de los peones blancos
-                    Alfiles[i]->PosicionHorizontal = 'b' + i*6;
+                    Alfiles[i]->PosicionHorizontal = 'c' + i*3;
                 }
             }
             else
                 exit(-1);
         }
         return Alfiles;
+    }
+    else
+        exit(-1);
+}
+
+DamaEstructura** DamaInicializador(uint8_t colorSeleccionado)
+{
+    //Alojar un apuntador de apuntadores de estructuras
+    DamaEstructura **Dama = calloc(sizeof(AlfilEstructura*), DamaNumero);
+
+    if(Dama)
+    {
+        //Alojar cada Dama inidividualmente
+        for(uint8_t i = 0; i<DamaNumero ; i++)
+        {
+            Dama[i] = calloc(sizeof(DamaEstructura),1);
+            if(Dama[i])
+            {
+                Dama[i]->Color = colorSeleccionado;
+                if (NEGRO == colorSeleccionado)
+                {
+                    Dama[i]->PosicionVertical = 1;     //Posiciones opr defecto de los peones negros
+                    Dama[i]->PosicionHorizontal = 'd';
+                }
+                else if (BLANCO == colorSeleccionado)
+                {
+                    Dama[i]->PosicionVertical = 8;     //Posiciones por defecto de los peones blancos
+                    Dama[i]->PosicionHorizontal = 'd';
+                }
+            }
+            else
+                exit(-1);
+        }
+        return Dama;
     }
     else
         exit(-1);
